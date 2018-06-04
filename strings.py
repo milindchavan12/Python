e5df39659dd5b3800a5f947590c2d167a7ef2c4a
Milind Chavan <Milind@Milinds-MacBook-Air.local>
2018-06-04 13:39:53 +0200

    With Strings

---
+++ strings.py
@@ -0,0 +1,11 @@
+print("Double quotes")
+
+print('Single quotes')
+
+print("Escape character : Does\'t")
+
+print('"string" with double quotes inside single quotes')
+
+print('Line 1 \nLine 2')
+
+print(r'Escape Charater in string : C:\Test\NewFile')
